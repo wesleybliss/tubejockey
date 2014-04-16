311cd8a74972dd415b3187074b6cc6fcdabfbdbe
Wesley Bliss <wesley.bliss@gmail.com>
2014-04-16 16:38:03 -0400

    Up & down keyboard navigation for search results working.

--- www/js/tubejockey-0.0.1.js
+++ www/js/tubejockey-0.0.1.js
@@ -1,76 +1,200 @@
+// @todo Use require.js/etc.
+if ( !jQuery || !$ ) {
+    console.error('This requires jQuery');
+}
+
+var l = function() { console.log(arguments); };
 
 /**
  * TubeJockey class
  */
-var TJ = function() {
-    
-    // @todo Use require.js/etc.
-    if ( !jQuery || !$ ) {
-        console.error( 'This requires jQuery' );
-    }
-    
-    this.HOST = 'http://tubejockey.local:8080';
-    this.ROUTES = {
+var TJ = tj = {
+
+    HOST: 'http://tubejockey.local:8080',
+    ROUTES: {
         SEARCH: 'search'
-    };
-    
+    },
+
     /**
      * Build a full API URI
      *
      * @param {String} paths    One or more paths to append to the base URI
      * @returns {String}
      */
-    this.buildURI = function( paths /*, [path2], [path3] ...*/ ) {
+    buildURI: function( paths /*, [path2], [path3] ...*/  ) {
         if ( !arguments || arguments.length < 1 ) {
             throw 'TJ::buildURI Path is required';
         }
-        return this.HOST + '/' +
+        return tj.HOST + '/' +
             Array.prototype.slice.call(arguments).join('');
-    };
-    
+    }
+
 };
 
+// DOM elements
+TJ.$query = null;
+TJ.$matches = null;
+TJ.KEYS = {
+    LEFT: 37,
+    RIGHT: 39,
+    UP: 38,
+    DOWN: 40
+};
+
+// Search timeout
+TJ.searchTimer = null;
 
 /**
  * Search for a video on YouTube, through the TJ API
  *
  * @param {String} query
- * @param {Function} done
+ * @param {Function} done Callback ( err, matches)
  */
-TJ.prototype.search = function( query, done ) {
-    //$.getJSON( this.buildURI(this.ROUTES.SEARCH), {query: query}, done );
-    
+TJ.search = function( query, done ) {
+    $.getJSON( tj.buildURI(tj.ROUTES.SEARCH), {
+        query: query
+    }, function( data ) {
+        done( data.err, data.data );
+    });
     return this;
 };
 
+TJ.handleSearch = function() {
 
+    tj.$matches.empty();
+    window.clearTimeout( tj.searchTimer );
 
-var $query = $('input#query');
+    // Delay search query to prevent the user from pissing off YouTube
+    tj.searchTimer = window.setTimeout( function(  ) {
+        
+        // @todo The scope is probably fucked right here, but it works for demo
+        tj.search( tj.$query.val(), function( err, data ) {
+            
+            if ( err ) {
+                console.log(err);
+                return false;
+            }
+            
+            $.each( data, function( index, item ) {
+                console.log( index + ': ' + item );
+                tj.$matches.append( '<li>' + item.name + '</li>' );
+                
+            });
+            
+            return tj;
+            
+        });
+        
+    }, 700);
+
+};
+
+/**
+ * Initializes the search handler on a given element
+ *
+ * @param {String} querySelector     DOM selector to find input element
+ * @param {String} matchesSelector   DOM selector to find results element ( only UL supported currently)
+ */
+TJ.init = function( querySelector, matchesSelector ) {
+
+    tj.$query = $(querySelector);
+    tj.$matches = $(matchesSelector);
+
+    tj.$matches.width( tj.$query.width() );
+
+    tj.$query.on( 'keyup', function(e) {
+
+        if ( [tj.KEYS.UP, tj.KEYS.DOWN].indexOf(e.keyCode) > -1 ) {
+            e.preventDefault();
+            console.log( 'key is up/down' );
+            tj.navigateMatches( e.keyCode );
+        }
+        else {
+            console.log( 'key is search' );
+            ( $(this).val( ) != $(this).data('last-value') ) &&
+                $(this).data( 'last-value', $(this).val() ) &&
+                tj.handleSearch();
+        }
+        
+    });
+    
+};
+
+/**
+ * Move faux highlight/focus up or down
+ *
+ * @param {Number} direction    TJ.KEYS up or down, or numeric key ID
+ */
+TJ.navigateMatches = function( direction ) {
+
+    switch ( direction ) {
+        
+        case tj.KEYS.UP:
+            tj.navigateUp();
+            break;
+        
+        case tj.KEYS.DOWN:
+            tj.navigateDown();
+            break;
+        
+    }
+    
+};
+
+TJ.navigateUp = function() {
+    
+    var $items = tj.$matches.find('li'),
+        $active = $items.filter('.selected'),
+        index = -1;
+    
+    $active = $active ? $active : $items.first();
+    index = $active.index();
+    
+    // Check for invalid index or index hitting beginning of list
+    if ( index <= 0 ) {
+        index = $items.length;
+    }
+    else if ( index >= ($items.length - 1) ) {
+        index = ( $items.length - 1 );
+    }
+    
+    $active.removeClass('selected');
+    index--;
+    
+    $active = $( $items.get(index) );
+    $active.addClass('selected');
+    
+    return tj;
+    
+};
+
+TJ.navigateDown = function() {
+    
+    var $items = tj.$matches.find('li'),
+        $active = $items.filter('.selected'),
+        index = -1;
+    
+    $active = $active ? $active : $items.first();
+    index = $active.index();
+    
+    // Check for invalid index or index hitting end of list
+    if ( index < 0 || index >= ($items.length - 1) ) {
+        index = -1;
+    }
+    
+    $active.removeClass('selected');
+    index++;
+    
+    $active = $( $items.get(index) );
+    $active.addClass('selected');
+    
+    return tj;
+    
+};
+
 
 $(function() {
     
-    var tj = new TJ();
-    
-    //$query.on( 'keyup', function(key) {
-    //    console.log( 'searching for ' + $(this).val() );
-    //    tj.search( $(this).val(), function(data) {
-    //        console.log( data );
-    //    });
-    //})
-    
-    
-    $query.tokenInput( tj.buildURI(tj.ROUTES.SEARCH), {
-        method: 'GET',
-        crossDomain: false,
-        queryParam: 'query',
-        jsonContainer: 'data',
-        propertyToSearch: 'name',
-        hintText: 'Search for music...',
-        resultsLimit: 10,
-        onReady: function() {},
-        onResult: function( matches ) {},
-        onAdd: function( item ) {},
-        onDelete: function( item ) {}
-    });
+    tj.init('input#query', 'ul#matches');
     
 });
